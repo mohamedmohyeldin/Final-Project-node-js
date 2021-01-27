c3545c69094e71e10a8e4988d5605b9b7b714d55
mohamedmohyeldin <76909633+mohamedmohyeldin@users.noreply.github.com>
2021-01-27 22:06:20 +0200

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,6 +10,7 @@ const auth=async(req,res,next)=>{
         const decode=jwt.verify(token,'news-app')
         const reporters= await Reporters.findOne({_id:decode._id,'tokens.token':token})
         console.log(reporters)
+      
         if(!reporters){
             throw new Error()
         }
