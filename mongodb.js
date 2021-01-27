9820114ab09b2bfcd532c34740cd659693ab4d4a
mohamedmohyeldin <76909633+mohamedmohyeldin@users.noreply.github.com>
2021-01-27 22:02:50 +0200

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,14 @@
 const mongodb = require("mongodb");
+
 const mongoClient = mongodb.MongoClient;
+
 const connectionUrl = "mongodb://127.0.0.1:27017";
+
+
 const dbName = "news-task";
+
+
+
 
 
 mongoClient.connect(
